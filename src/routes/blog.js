1baee192eae4bdde49223402271257b8d2404e29
Victor <zedfromionia@gmail.com>
2020-02-23 16:18:13 -0300

    new delete and put routes, added tests to get and post routes

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,18 +1,69 @@
 const Router = require('express').Router();
 const Blog = require('../models/Blog');
 
-Router.get('/blogs', (req, res) => {
-    Blog.find({}).then(blogs => {
+Router.get('/blogs', async (req, res) => {
+    try {
+        const blogs = await Blog.find({});
+
         res.json(blogs);
-    });
+    } catch (err) {
+        console.log('Error:', err);
+        res.status(400).end();
+    }
 });
 
-Router.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
+Router.post('/blogs', async (req, res) => {
+    try {
+        let { title, author, url, likes } = req.body;
 
-    blog.save().then(result => {
+        if (typeof title === 'undefined' || typeof url === 'undefined') {
+            return res.status(400).end();
+        }
+
+        if (typeof likes === 'undefined') {
+            likes = 0;
+        }
+
+        const blog = new Blog({ title, author, url, likes });
+
+        const result = await blog.save();
+
         res.status(201).json(result);
-    });
+    } catch (err) {
+        console.log('Error', err);
+        res.status(400).end();
+    }
+});
+
+Router.delete('/blogs/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        await Blog.findByIdAndDelete({ _id: id });
+
+        res.status(204).end();
+    } catch (err) {
+        console.log(err);
+        res.status(400).end();
+    }
+});
+
+Router.put('/blogs/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, author, url, likes } = req.body;
+
+        const updatedBlog = await Blog.findByIdAndUpdate(
+            id,
+            { title, author, url, likes },
+            { new: true }
+        );
+
+        res.json(updatedBlog);
+    } catch (err) {
+        console.log(err);
+        res.status(400).end();
+    }
 });
 
 module.exports = Router;
